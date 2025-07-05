75aa76fca1981d8e9ac177bcc1279dc02fd82e59
MAC <ahmed.hassaine432@gmail.com>
2025-07-05 15:01:08 +0100

    starting to place the individual projects in their place

---
+++ apps/dashboard/app/(tabs)/index.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+import { View, Text } from "react-native"
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hell world</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -9,6 +9,7 @@ interface ButtonProps {
 }
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
+  console.log("hello world")
   return (
     <button
       className={className}
