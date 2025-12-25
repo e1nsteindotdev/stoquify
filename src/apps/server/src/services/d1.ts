9f7b0bef0326c19cc76aa1c0be2d20fdbc382224
MAC <ahmed.hassaine432@gmail.com>
2025-12-25 23:01:25 +0100

    cleaned stuff app

--- src/apps/server/src/services/d1.ts
+++
@@ -1,6 +0,0 @@
-import { Context } from "effect"
-
-export class D1 extends Context.Tag("D1")<
-  D1,
-  D1Database
->() { }
--- src/apps/server/src/utils/client-rpc.ts
+++
@@ -1,4 +0,0 @@
-import { Api } from "../index.js"
-import { HttpApiClient } from "@effect/platform"
-
-export const api = HttpApiClient.make(Api, { baseUrl: "http://localhost:3000" })
